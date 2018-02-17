196dc404d121f08a611ed5be61114fb3efe2135a
Matthew Miller <matthew@mi11er.net>
2018-02-16 20:22:09 -0500

    temp

---
+++ preact.config.js
@@ -0,0 +1,15 @@
+// import path from 'path';
+
+// export default function(config, env, helpers) {
+// console.log(config);
+// helpers.getPluginsByName(config,'HtmlWebpackPlugin').forEach(a=>{
+//  console.log(a.plugin.options.ssr)});
+// console.log(helpers.getLoaders(config));
+// const oldBuildPath = env.dest;
+// const newBuildPath = path.resolve(env.cwd, 'dist');
+// env.dest=newBuildPath;
+// config.output.path = config.output.path.replace(oldBuildPath,newBuildPath);
+// console.log(config);
+// console.log(env);
+// console.log(env);
+// }
